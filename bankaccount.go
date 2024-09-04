4225e65f417bdacce494f770f50f0cff0c03321a
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-09-04 05:08:50 +0200

    first menu logic

--- bankaccount.go
+++ bankaccount.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var option int
+	for {
+		showOptions()
+		fmt.Scan(&option)
+		if(option == 4) {
+			fmt.Println("Goodbye")
+			break
+		}
+	}
+	
+}
+
+func showOptions() {
+	fmt.Println("\nMain Menu")
+	fmt.Println("1) check balance")
+	fmt.Println("2) deposit money")
+	fmt.Println("3) withdraw money")
+	fmt.Println("4) exit")
+	fmt.Print("choice: ")
 }
