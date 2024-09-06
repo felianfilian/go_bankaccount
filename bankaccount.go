1e4948f9e0555653ceac530f80bcf9aff49a04b3
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-09-06 12:02:39 +0200

    write data to file

--- bankaccount.go
+++ bankaccount.go
@@ -7,8 +7,9 @@ import (
 
 func main() {
 	var option int
+	var balance float64 
 	for {
-		showOptions()
+		showOptions(balance)
 		fmt.Scan(&option)
 
 		// switch option {
@@ -19,7 +20,9 @@ func main() {
 		if(option == 1) {
 
 		} else if(option == 2) {
-			
+			fmt.Scan(&balance)
+			writeToFile(balance)
+
 		} else if(option == 4) {
 			fmt.Println("Goodbye")
 			break
@@ -28,7 +31,9 @@ func main() {
 	
 }
 
-func showOptions() {
+func showOptions(balance float64) {
+	fmt.Print("Actual Balace: ")
+	fmt.Print(balance)
 	fmt.Println("\nMain Menu")
 	fmt.Println("1) check balance")
 	fmt.Println("2) deposit money")
